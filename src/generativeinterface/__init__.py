eb55eb07f71d8bab63d870fc0673785b1a9ee9d6
Francisco <up201705031@fe.up.pt>
2024-03-07 09:02:15 +0100

    Setup cookiecutter

---
+++ src/generativeinterface/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for generativeinterface.
+
+    Interface to interact with several generative models.
+"""
+from version import __version__
+
+__author__ = "Francisco Caetano"
